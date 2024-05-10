a517d2fbe5edade267f05a96cf23d4d47a83b6db
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-05-10 21:25:29 +0800

    yes

--- clicker.py
+++ clicker.py
@@ -9,15 +9,13 @@ import time
 
 running = False
 that_thread = None
+x = 0
+y = 0
 
 def click():
     global running
-
-    location = pyautogui.locateOnScreen('cogtwo.png')
-    location = pyautogui.size()
-    x = int(location.width/2)
-    y = int(location.height/2)
-    #pydirectinput.click(x,y, clicks=5)
+    global x
+    global y
 
     while running:
         autoit.mouse_click("left", x, y, 1, 0)
@@ -27,8 +25,19 @@ def click():
 def check_thread():
     global running
     global that_thread
+    global x 
+    global y
 
     if not running:
+        try:
+            location = pyautogui.locateOnScreen('cogtwo.png')
+            location = pyautogui.size()
+            x = int(location.width/2)
+            y = int(location.height/2)
+            #pydirectinput.click(x,y, clicks=5)
+        except:
+            print("error")
+            return
         print("Starting")
         running = True
         that_thread = threading.Thread(target=click)
